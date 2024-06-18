4427c2f35441b7bcb1685830793de0e9cb1fcaa5
Arul Mozhi Varman G <113763583+Ganeshan12@users.noreply.github.com>
2024-06-18 19:20:28 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+let movies = [{
+        name: ' Falcon and the Winter soldier',
+        des: 'Experience the thrilling action and deep storytelling of Marvel\'s latest miniseries. Join Sam Wilson and Bucky Barnes as they navigate a world without Captain America, facing new challenges and forging their paths as heroes. Stream now on Disney+ for an unforgettable adventure!',
+        image: 'images/slider 2.jpg'
+    },
+    {
+        name: ' Loki',
+        des: 'Get ready for mischief and mayhem with Loki, the God of Mischief, in his own original series. Join Loki as he steps out of his brother Thor\'s shadow and embarks on a thrilling adventure across time and space. With the fate of reality at stake, Loki must confront his past and embrace his destiny. Prepare for an epic journey filled with twists, turns, and surprises. Stream all episodes now on Disney+!',
+        image: 'images/slider 1.jpg'
+    },
+    {
+        name: ' Wanda Vision',
+        des: 'Step into the enchanting and mysterious world of WandaVision, a Marvel Studios series on Disney+. Join Wanda Maximoff and Vision as they navigate a surreal suburban life that’s full of unexpected twists and turns. Experience a unique blend of classic television and the Marvel Cinematic Universe in this captivating adventure. Stream now on Disney+!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: ' Raya and the Last dragon',
+        des: ' Embark on an epic quest with Raya, a fearless warrior, and her trusty sidekick Tuk Tuk, as they journey through the mystical realm of Kumandra to find the last dragon. Set in a vibrant and lush world inspired by Southeast Asian cultures, Raya and the Last Dragon is an action-packed adventure filled with humor, heart, and breathtaking animation. Join Raya on her quest to unite the fractured land and restore peace to the kingdom. Stream now on Disney+!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: ' Luca',
+        des: 'Dive into the vibrant and sun-soaked world of Luca, a heartwarming tale from Disney and Pixar. Follow the unforgettable summer adventures of a young boy on the Italian Riviera, where friendship, fun, and a hidden secret come together. Join Luca and his friends as they explore new horizons and discover the true meaning of friendship. Stream now on Disney+!',
+        image: 'images/slider 5.PNG'
+    }
+]
